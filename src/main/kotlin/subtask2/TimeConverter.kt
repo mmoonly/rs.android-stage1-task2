27581be36082da8c89191137e31b93f9ab7109ee
mmoonly <61161823+mmoonly@users.noreply.github.com>
2020-04-12 12:10:46 +0300

    Task 2 Complete

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -4,6 +4,47 @@ class TimeConverter {
 
     // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val hArray = arrayOf("zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve")
+        val mArray = arrayOf(
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+            "twenty",
+            "twenty one",
+            "twenty two",
+            "twenty three",
+            "twenty four",
+            "twenty five",
+            "twenty six",
+            "twenty seven",
+            "twenty eight",
+            "twenty nine",
+            "thirty")
+        return when (minute.toInt()) {
+            0 -> "${hArray[hour.toInt()]} o' clock"
+            15 -> "quarter past ${hArray[hour.toInt()]}"
+            30 -> "half past ${hArray[hour.toInt()]}"
+            45 -> "quarter to ${hArray[hour.toInt() + 1]}"
+            1 -> "one minute past ${hArray[hour.toInt()]}"
+            in 1..30 -> "${mArray[minute.toInt() - 1]} minutes past ${hArray[hour.toInt()]}"
+            in 31..60 -> "${mArray[60 - minute.toInt() - 1]} minutes to ${hArray[hour.toInt() + 1]}"
+            else -> ""
+        }
     }
 }
